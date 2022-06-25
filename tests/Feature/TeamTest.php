2bf5ccbf40bca8a84cd16273e6aa27773d4a1685
2113-dm <diana.ledezma@ucr.ac.cr>
2022-06-24 20:32:37 -0600

    resolucion de tarea 1 (B84224)

--- tests/Feature/TeamTest.php
+++ tests/Feature/TeamTest.php
@@ -45,10 +45,8 @@ class TeamTest extends TestCase
 
         $user = User::factory()->create();
         $user2 = User::factory()->create();
-
         $team->add($user);
         $team->add($user2);
-
         $this->assertEquals(2, $team->count());
 
         $this->expectException('Exception');
@@ -62,12 +60,33 @@ class TeamTest extends TestCase
     /** @test */
     public function un_equipo_puede_excluir_un_usuario()
     {
-        # code...
+       
+        $team = Team::factory()->create(['size' => 1]);
+
+        $this->withoutExceptionHandling();
+
+        $user = User::factory()->create();
+
+        $response = $this->delete($user);
+
+        $this->assertEquals(0, $team->count());
+       
     }
 
     /** @test */
     public function un_equipo_puede_excluir_todos_los_usuarios_a_la_vez()
     {
-        # code...
+        $team = Team::factory()->create(['size' => 2]);
+
+        $this->withoutExceptionHandling();
+
+        $user = User::factory()->create();
+
+        $user2 = User::factory()->create();
+
+        $response = $this->delete($user);
+        
+        $this->assertEquals(0, $team->count());
+
     }
 }
