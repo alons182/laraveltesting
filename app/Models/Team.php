3f3095ab7774d433642b5ab6da0cd20039e61281
CAMILA DE JESUS ZAMORA CUBILLO <camila.zamora@ucr.ac.cr>
2022-06-24 22:38:42 -0600

    resolucion de tarea

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -1,25 +1,28 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
+
+
 
 class Team extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     protected $fillable = ['name'];
 
     public function add($user)
     {
-       
+
         $this->preventTooManyUsers();
 
         $method = $user instanceof User ? 'save' : 'saveMany';
 
         return $this->members()->$method($user);
-        
+
         // if($user instanceof User){
         //     return $this->members()->save($user);
         // }
@@ -27,15 +30,20 @@ class Team extends Model
         // return $this->members()->saveMany($user);
     }
 
+
+
     public function members()
     {
         return $this->hasMany(User::class);
     }
 
+
     public function count()
     {
         return $this->members()->count();
     }
+
+
 
     public function preventTooManyUsers()
     {
@@ -43,5 +51,31 @@ class Team extends Model
             throw new \Exception('Ohh Error');
         }
     }
+
+
+
+    public function unsetUser($user)
+    {
+        $method = $user instanceof User ? 'destroy' : 'saveMany';
+        return $this->members()->$method($user);
+    }
+
+
+    public function unsetAllUsers($user)
+    {
+        $method = $user instanceof User ? 'delete' : 'saveMany';
+        return $this->members()->$method($user);
+    }
+
+
+    public function softDelete($user)
+    {
+        foreach ($user as $team) {
+            echo $user->name;
+            $user->softDeletes();
+        }
+    }
+
+
 
 }
--- tests/Feature/TeamTest.php
+++ tests/Feature/TeamTest.php
@@ -5,46 +5,45 @@ namespace Tests\Feature;
 use App\Models\Team;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class TeamTest extends TestCase
 {
     use RefreshDatabase;
-    
-    
+
+
     /** @test */
     public function un_equipo_puede_agregar_usuario()
     {
         $team = Team::factory()->create();
         $user = User::factory()->create();
         $user2 = User::factory()->create();
-
         $team->add($user);
         $team->add($user2);
-
         $this->assertEquals(2, $team->count());
-       
     }
+
     /** @test */
     public function un_equipo_puede_agregar_multiples_usuarios_a_la_vez()
     {
-       $team = Team::factory()->create();
-       $users = User::factory(2)->create();
-
-       $team->add($users);
-
-       $this->assertEquals(2, $team->count());
-
+        $team = Team::factory()->create();
+        $users = User::factory(2)->create();
+        $team->add($users);
+        $this->assertEquals(2, $team->count());
     }
 
     /** @test */
     public function un_equipo_tiene_un_maximo_usuarios()
     {
+
         $team = Team::factory()->create(['size' => 2]);
 
         $user = User::factory()->create();
         $user2 = User::factory()->create();
+
+        //$team->delete($team[1]);
 
         $team->add($user);
         $team->add($user2);
@@ -59,15 +58,68 @@ class TeamTest extends TestCase
 
     }
 
+
+
+
+
     /** @test */
     public function un_equipo_puede_excluir_un_usuario()
     {
-        # code...
+        //creamos el team y los user
+        $team = Team::factory()->create();
+
+        $user = User::factory()->create();
+        $user2 = User::factory()->create();
+        //$team->unset($team[1]);
+        $team->add($user);
+        $team->add($user2);
+        //agregamos los user al team
+
+        //eliminamos los users
+        $team->unsetUser($user2);
+        $this->assertEquals(1, $team->count());
     }
+
+
 
     /** @test */
     public function un_equipo_puede_excluir_todos_los_usuarios_a_la_vez()
     {
-        # code...
+        //creamos el team y los user
+        $team = Team::factory()->create();
+
+        $user = User::factory()->create();
+        $user2 = User::factory()->create();
+        //$team->delete($team[1]);
+        $team->add($user);
+        $team->add($user2);
+
+        //eliminamos los users
+        $team->unsetAllUsers($user2, $user);
+
+        $this->assertEquals(0, $team->count());
+
     }
+
+
+//  /** @test */
+//     public function un_equipo_puede_excluir_todos_los_usuarios_a_la_vez()
+//     {
+//         //creamos el team y los user
+//         $team = Team::factory()->create();
+
+//         $user = User::factory()->create();
+//         $user2 = User::factory()->create();
+//         //$team->delete($team[1]);
+//         $team->add($user);
+//         $team->add($user2);
+
+//         //eliminamos los users
+
+//         $this->assertEquals(0,  $team-> unsetAllUsers($user2, $user));
+
+//     }
+
+
+
 }
